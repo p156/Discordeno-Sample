60f1168c1a25f543256ab688e85c91559df86171
p156 <38420559+p156@users.noreply.github.com>
2025-07-23 04:12:28 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
 import "$std/dotenv/load.ts"
-import { createBot,getBotIdFromToken,startBot } from '@discordeno/mod.ts'
+import { createBot } from '@discordeno/mod.ts'
 
 const BotToken: string = Deno.env.get("BOT_TOKEN")!;
 
-const bot = createBot({
-    token: BotToken,
-    botId: getBotIdFromToken(BotToken) as bigint,
-});
-await startBot(bot);
+await bot.start()
 
 Deno.cron("Continuous Request", "*/2 * * * *", () => {
     console.log("running...");
